1513d465bd4643031c6f8d3d41fc6f52427e4402
JMI <ikolajm@gmail.com>
2019-03-10 13:12:40 -0400

    more simple solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,13 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppComponent {
   title = 'SWAPI App';
-  _dropdown = '';
-  _searchField = '';
   _data = {};
   baseUrl = 'https://swapi.co/api'
 
   constructor(private http: HttpClient) { }
-
-  onType(dropdown, searchField) {
-    this._dropdown = dropdown;
-    this._searchField = searchField;
-  }
   
-  onSubmit() {
-    return (this.http.get(`${this.baseUrl}/${this._dropdown}/?search=${this._searchField}`))
+  onSubmit(dropdown, search) {
+    return (this.http.get(`${this.baseUrl}/${dropdown}/?search=${search}`))
       .subscribe(data => {
         this._data = data;
       })
